872bef2d0d2081db8e0bbb5dfa6aef48351eadc4
lua <tomghallen@gmail.com>
2025-11-15 13:43:06 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -21,6 +21,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua
 sendNotification("thread.lua", "Inventory checking loaded!", 3)
 sendNotification("thread.lua", "Loading complete! Press F9 to view controls.", 3)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua/refs/heads/main/threadcleanseview.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua/refs/heads/main/threadanim.lua"))()
 
 print([[
 
@@ -53,5 +54,3 @@ Scroll Wheel - Aimbot (Toggle)
 =============================================
 
 ]])
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua/refs/heads/main/threadanim.lua"))()
