c80eba1898ea444937271f2376d4e69e3fb60c0f
lua <tomghallen@gmail.com>
2025-11-15 13:42:30 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -54,4 +54,4 @@ Scroll Wheel - Aimbot (Toggle)
 
 ]])
 
-loadtsring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua/refs/heads/main/threadanim.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua/refs/heads/main/threadanim.lua"))()
