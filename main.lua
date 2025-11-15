5e43be50d095a5e34332e43a4cc53dd6e259cc75
lua <tomghallen@gmail.com>
2025-11-15 13:28:18 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -19,8 +19,8 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua
 sendNotification("thread.lua", "Skeleton + Drawing loaded!", 3)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua/refs/heads/main/invchecker.lua"))()
 sendNotification("thread.lua", "Inventory checking loaded!", 3)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua/refs/heads/main/threadcleanseview.lua"))()
 sendNotification("thread.lua", "Loading complete! Press F9 to view controls.", 3)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua/refs/heads/main/threadcleanseview.lua"))()
 
 print([[
 
