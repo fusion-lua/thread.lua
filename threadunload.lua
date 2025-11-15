babc44b16bf46d47726b8ffa195ff222f6f1ad2b
lua <tomghallen@gmail.com>
2025-11-15 13:55:58 +0000

    Create threadunload.lua

---
+++ threadunload.lua
@@ -0,0 +1,11 @@
+local f5Connection
+f5Connection = UserInputService.InputBegan:Connect(function(input, processed)
+    if processed then return end
+    if input.KeyCode == Enum.KeyCode.F5 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fusion-lua/thread.lua/refs/heads/main/threadanim.lua"))()
+        if f5Connection then
+            f5Connection:Disconnect()
+            f5Connection = nil
+        end
+    end
+end)
